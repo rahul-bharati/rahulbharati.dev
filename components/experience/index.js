a9899dd887f9824797b0a80efdb42465ae14c8c5
Rahul Bharati <rahulrbharati@gmail.com>
2022-08-11 00:17:05 +0530

    update work experience

--- components/experience/index.js
+++ components/experience/index.js
@@ -81,7 +81,7 @@ function Experience() {
         variants={detailsVariant}
       >
         <h3 className={styles.skill__heading}>
-          Web Developer - Rapidera Technologies (February 202022 - Present)
+          Web Developer - Rapidera Technologies (February 2022 - August 2022)
         </h3>
         <span className={styles.skill__text}>
           <ul>
